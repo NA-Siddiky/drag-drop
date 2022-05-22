b6a76af3c68f8feeefe64fe0b95df702cf68a5ff
NA-Siddiky <siddiky.academic@gmail.com>
2022-05-23 01:26:18 +0600

    besic function

--- src/components/drop-file-input/DropFileInput.jsx
+++ src/components/drop-file-input/DropFileInput.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useRef, useState } from 'react'
 import PropTypes from 'prop-types'
 
 import './drop-file-input.css';
@@ -8,8 +8,42 @@ import { ImageConfig } from '../../config/ImageConfig';
 import uploadImg from '../../assets/cloud-upload-regular-240.png';
 
 const DropFileInput = props => {
+
+    const wrapperRef = useRef(null);
+    const [fileList, setFileList] = useState([]);
+
+
+    const onDragEnter =()=> wrapperRef.current.classList.add('dragover');
+
+    const onDragLeave =()=> wrapperRef.current.classList.remove('dragover');
+
+    const onDrop = () => wrapperRef.current.classList.remove('dragover');
+
+    // const onFileDrop = (e) => {
+    //     const newFile = e.target.files[0];
+    //     if (newFile) {
+    //         const updatedList = [...fileList, newFile];
+    //         setFileList(updatedList);
+    //         props.onFileChange(updatedList);
+    //     }
+    // }
+
+    // const fileRemove = (file) => {
+    //     const updatedList = [...fileList];
+    //     updatedList.splice(fileList.indexOf(file), 1);
+    //     setFileList(updatedList);
+    //     props.onFileChange(updatedList);
+    // }
+
+
   return (
-    <div    className="drop-file-input">
+    <div  
+    ref={wrapperRef}
+    className="drop-file-input"
+    onDragEnter={onDragEnter}
+    onDragLeave={onDragLeave}
+    onDrop={onDrop}
+      >
     <div className="drop-file-input__label">
         <img src={uploadImg} alt="" />
         <p>Drag & Drop your files here</p>
