4ca80b26a587fe9bec2c4ed805c70f6c2bc7cc68
NA-Siddiky <siddiky.academic@gmail.com>
2022-05-22 18:45:13 +0600

    basic done

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
-import './main.css';
+import React from 'react';
+import Board from './components/Board';
+import Card from './components/Card';
+
 
 function App() {
   return (
     <div className="App">
-      <h1>Test</h1>
+      <h1>Drag & Drop</h1>
       <main className="flexbox">
+        <Board id="board-1" className="board">
+          <Card id="card-1" className="card" draggable="true">
+            <p>Card One</p>
+          </Card>
+        </Board>
+
+        <Board id="board-2" className="board">
+          <Card id="card-2" className="card" draggable="true">
+            <p>Card Two</p>
+          </Card>
+        </Board>
 
       </main>
     </div>
